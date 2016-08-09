4fac5a3fa3f6fea68003da27fc8ac3f01b59c950
Marlo Bennett <marlomariebennett@gmail.com>
2016-08-08 19:02:41 -0500

    updated html and js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,17 @@
 // Sets the computer choices 
 var wordBank = ["doberman", "labrador", "beagle", "bulldog", "akita", "husky", "pomeranian", "yorkie", "mastiff", "pug", "malamute", "collie", "bloodhound", "boxer", "sheepdog", "dalmation", "greyhound"];
 
-// Declares the tallies to 0 
 var wins = 0;
 
 var lettersGuessed = "";
+
 var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 
 // When the user presses the key it records the keypress and then sets it to userguess
 document.onkeyup = function(event) {
 	var currentLetterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
 	lettersGuessed = lettersGuessed + currentLetterGuessed;
+	var guessesRemaining = currentWord.length + 10;
 
 	console.log(currentWord);
 
@@ -19,10 +20,11 @@ document.onkeyup = function(event) {
 		//Taking the tallies and displaying them in HTML
 	var html = 
 		"<p>Press any letter key to start playing.</p>" +
-		"<p>Letters guessed: " + lettersGuessed+ "</p>" +
+		"<p>Letters guessed: " + lettersGuessed + "</p>" +
 		"<p>Hangman word: " + hangmanWord + "</p>" +
+		"<p>Guesses remaining: " + guessesRemaining + "</p>" + 
 		"<p>wins: " + wins + "</p>";
-	
+
 	if (lettersGuessed.length > currentWord.length + 10) {
 		html = html + "The man is hanged! You lose."
 	}
@@ -36,8 +38,6 @@ document.onkeyup = function(event) {
 	document.querySelector("#game").innerHTML = html;
 
 }
-
-	
 
 function getHangmanWord(lettersGuessed) {
 	
