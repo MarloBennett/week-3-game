8eef3df15281e5921b766c9e4392e544bf021443
Marlo Bennett <marlomariebennett@gmail.com>
2016-08-10 22:33:51 -0500

    updated js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,8 @@
 // Sets the computer choices 
 var wordBank = ["doberman", "labrador", "beagle", "bulldog", "akita", "husky", "pomeranian", "yorkie", "mastiff", "pug", "malamute", "collie", "bloodhound", "boxer", "sheepdog", "dalmation", "greyhound"];
 
+var possibleGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 var wins = 0;
 
 var lettersGuessed = "";
@@ -12,7 +14,33 @@ var guessesRemaining = currentWord.length + 7;
 // When the user presses the key it records the keypress and then sets it to userguess
 document.onkeyup = function(event) {
 	var currentLetterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-	lettersGuessed = lettersGuessed + currentLetterGuessed;
+
+	
+	for (c = 0; c < possibleGuesses.length; c++) {
+		var possibleChoice = possibleGuesses[c]; 
+
+		//iterates over array of possible choices (letters only); adds to lettersGuessed if so
+		if (currentLetterGuessed.indexOf(possibleChoice) > -1) {
+			lettersGuessed = lettersGuessed + currentLetterGuessed;
+		}
+		//if not a letter, doesn't add to lettersGuessed
+		else {
+			lettersGuessed = lettersGuessed;
+		}
+	}
+
+	for (d = 0; d < lettersGuessed.length; d++) {	
+		var alreadySelected = lettersGuessed[d]; 
+
+		//iterates over array of lettersGuessed; adds to lettersGuessed if not already chose
+		if (currentLetterGuessed.indexOf(alreadySelected) < 0) {
+			lettersGuessed = lettersGuessed + currentLetterGuessed;
+		}
+		//if already selected, doesn't add to lettersGuessed
+		else {
+			lettersGuessed = lettersGuessed;
+		}
+	}
 
 	console.log(currentWord);
 
@@ -51,7 +79,7 @@ function getHangmanWord(lettersGuessed) {
 	for (i = 0; i < currentWord.length; i++) {
 		var currentWordLetter = currentWord[i];
 
-		// If the current word letter is in the string of letters guessed...
+		// If the current word letter is in the string of letters guessed
 		if (lettersGuessed.indexOf(currentWordLetter) > -1 ) {
 			hangmanWord = hangmanWord + currentWordLetter;
 		}
