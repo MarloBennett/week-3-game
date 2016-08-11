75c525c038d0a948dae214e19a2769050fa3f16f
Marlo Bennett <marlomariebennett@gmail.com>
2016-08-11 18:31:13 -0500

    updated js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,116 @@
 // Sets the computer choices 
-var wordBank = ["doberman", "labrador", "beagle", "bulldog", "akita", "husky", "pomeranian", "yorkie", "mastiff", "pug", "malamute", "collie", "bloodhound", "boxer", "sheepdog", "dalmation", "greyhound"];
-
-var possibleGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wordBank = ["doberman", "labrador", "corgi", "beagle", "bulldog", "akita", "husky", "pomeranian", "yorkie", "mastiff", "pug", "malamute", "collie", "bloodhound", "boxer", "sheepdog", "dalmation", "greyhound"];
 
 var wins = 0;
 
 var lettersGuessed = "";
 
-var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+// Set to true if a game is over and should restart when key pressed
+var restartGame = false;
 
-var guessesRemaining = currentWord.length + 7;
+var currentWord;
+
+var guessesRemaining;
+
+var msg = "";
+
+// Init the first game when the page is loaded
+initializeNewGame();
+
+//Initializes a new game
+function initializeNewGame() {
+
+	console.log("Initializing new game");
+
+	restartGame = false;
+
+	// Choose the word to be guessed
+	currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+	console.log(currentWord);
+
+	// Set the number of guesses remaining
+	guessesRemaining = currentWord.length + 7;
+
+	// Clear the letters guessed
+	lettersGuessed = "";	
+
+	//resets message to nothing
+	msg = "";
+
+	updateGame();
+}
+
 
 // When the user presses the key it records the keypress and then sets it to userguess
 document.onkeyup = function(event) {
+	
+	if (restartGame) {
+		initializeNewGame();
+		return;
+	}
+
 	var currentLetterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
 
+	// If the key pressed is not a letter, tell them and don't add to letters chosen
+	if (!(currentLetterGuessed.match(/[a-z]/i))) { 
+		msg = "<p>Your choice is not a letter!</p>";
+	} 
+	// else, if the letter has already been chosen, tell them and dont't add to letters chosen
+	else if (lettersGuessed.indexOf(currentLetterGuessed)>-1) {
+		msg = "<p>You already tried that letter! Please choose a different letter.</p>"
+	}
+	// otherwise, the letter is a valid guess
+	else {
+		// Check if the letter is in the word. Tell them if not. Remove a guess if not.
+		if(currentWord.indexOf(currentLetterGuessed)<0) {
+			msg = "Sorry! Your man is one part closer to being hanged.";	
+			guessesRemaining--;
+		}
+		else {
+			msg = "Good call!";	
+		}
+		
+		lettersGuessed = lettersGuessed + currentLetterGuessed;
+	}
+
+	updateGame();
 	
-	for (c = 0; c < possibleGuesses.length; c++) {
-		var possibleChoice = possibleGuesses[c]; 
+}
 
-		//iterates over array of possible choices (letters only); adds to lettersGuessed if so
-		if (currentLetterGuessed.indexOf(possibleChoice) > -1) {
-			lettersGuessed = lettersGuessed + currentLetterGuessed;
-		}
-		//if not a letter, doesn't add to lettersGuessed
-		else {
-			lettersGuessed = lettersGuessed;
-		}
-	}
 
-	for (d = 0; d < lettersGuessed.length; d++) {	
-		var alreadySelected = lettersGuessed[d]; 
+function updateGame() {
 
-		//iterates over array of lettersGuessed; adds to lettersGuessed if not already chose
-		if (currentLetterGuessed.indexOf(alreadySelected) < 0) {
-			lettersGuessed = lettersGuessed + currentLetterGuessed;
-		}
-		//if already selected, doesn't add to lettersGuessed
-		else {
-			lettersGuessed = lettersGuessed;
-		}
-	}
-
-	console.log(currentWord);
+	console.log("Upating game");
 
 	var hangmanWord = getHangmanWord(lettersGuessed);	
 
 		//Taking the tallies and displaying them in HTML
-	var html = 
-		"<p>Press any letter key to start playing.</p>" +
+	var html = msg + 
 		"<p>Letters guessed: " + lettersGuessed + "</p>" +
 		"<p>Hangman word: " + hangmanWord + "</p>" +
-		"<p>Guesses remaining: " + guessesRemaining + "</p>"; 
+		"<p>Guesses remaining: " + guessesRemaining + "</p>";
 
-	if (lettersGuessed.length > currentWord.length + 6) {
-		html = html + "The man is hanged! You lose.";
-		setTimeout(function() {location.reload(false)}, 3000);
-	}
-	else if (hangmanWord.indexOf("_") < 0) {
+	// Check if the player won
+	if (hangmanWord.indexOf("_") < 0) {
 		wins++;
-		html = html + "<p>You win!</p>" +
-		"<p>Wins: " + wins + "</p>";
-		setTimeout(function() {location.reload(false)}, 3000);
+		html = html + "<p>You win! Press any key to start a new game.</p>" +
+					"<p>Wins:" + wins;
+		restartGame = true;
 	} 
-	console.log("wins:" + wins);
-	
+	// Check if the player lost
+	else if (guessesRemaining < 1){
+		html = html + "<p>Your man is hanged! Press any key to start a new game.</p>"
+		restartGame = true;
+	}
+	// Otherwise, keep going
+	else {
+		html = html + "<p>Select a new letter to continue.</p>";
+	}
+
 	// Placing the html into the game ID
 	document.querySelector("#game").innerHTML = html;
-
-
+}
 
 
 function getHangmanWord(lettersGuessed) {
@@ -88,8 +130,6 @@ function getHangmanWord(lettersGuessed) {
 			hangmanWord = hangmanWord + "_ ";
 		}
 	}
-	guessesRemaining--;
 	
 	return hangmanWord;
-}
 }
